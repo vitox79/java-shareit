d940b19666649074da74a9ce7ed5ad2451e12b89
vitox79 <viktor.monarev@gmail.com>
2023-05-28 04:06:29 +0600

    share-it2

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,17 +1,19 @@
 package ru.practicum.shareit.booking;
 
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Booking {
     public enum Status {WAITING, APPROVED, REJECTED, CANCELED}
+
     private long id;
     private LocalDateTime start;
     private LocalDateTime end;
