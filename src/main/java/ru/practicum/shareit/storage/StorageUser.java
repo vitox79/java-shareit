edfa19d7aee34277a720cb1c8f7ae01359c59010
vitox79 <viktor.monarev@gmail.com>
2023-06-01 21:09:42 +0600

    review2 checkstyle

--- src/main/java/ru/practicum/shareit/storage/StorageUser.java
+++ src/main/java/ru/practicum/shareit/storage/StorageUser.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.user.User;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 @Component
--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -1,14 +1,11 @@
 package ru.practicum.shareit.user.service;
 
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.exception.model.DataNotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.storage.StorageUser;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
