a0828eaa9f7b9b92918c80e1d4f98b55c09de1e6
vitox79 <viktor.monarev@gmail.com>
2023-07-09 00:31:56 +0600

    dockerExceptionValidation

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandlerController.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandlerController.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,12 +31,22 @@ public class ErrorHandlerController {
         );
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException e) {
+
+        return Map.of(
+            "error", "Validation failed",
+            "details", e.getMessage()
+        );
+    }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleInternalServerException(final Exception e) {
         log.error("500 Internal Server Error", e);
         return Map.of(
-            "error", e.getMessage()
+            "eRRor ", e.getMessage()
         );
     }
 
